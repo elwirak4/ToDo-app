4dc5a6a545a8ab17209c77f1069c5269055c48e8
elwirak4 <kobusevich@gmail.com>
2018-08-18 10:20:47 +0200

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+//Check of Specfic Todo by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("js--completed");
+  
+});
+//Click on span to delete Todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+       $(this).remove(); 
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo text from imput
+        var todoText = $(this).val(); 
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle(500);
+});
